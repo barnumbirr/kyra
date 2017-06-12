6c56d7c5c0474392b4c0ad52e295c5554c11160f
Martin Simon <martin@tentwentyfour.lu>
2017-06-12 14:59:09 +0200

    v.0.4 - small steps

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 <h1><img src="https://raw.githubusercontent.com/c0ding/kyra/master/doc/kyra.png" height=55 alt="kyra" title="kyra"> kyra</h1>
 
-[![PyPi Version](http://img.shields.io/pypi/v/kyra.svg)](https://pypi.python.org/pypi/kyra/)   [![Downloads](http://img.shields.io/pypi/dm/kyra.svg)](https://pypi.python.org/pypi/kyra/)
+[![PyPi Version](https://img.shields.io/pypi/v/kyra.svg)](https://pypi.python.org/pypi/kyra/)
 
-**kyra** is a small logging library written in Python I made for my own use.
+**kyra** is a small logging library written in Python I made for my own use. This library has been tested with Python 2.7.x and Python 3.6.x.
 
 ## Installation:
 
@@ -37,20 +37,19 @@ log.error('This tests the ERROR logging call.')
 ## License:
 
 ```
-  Apache v2.0 License
-  Copyright 2014-2017 Martin Simon
+Copyright 2014-2017 Martin Simon
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+   http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 ```
 
--- kyra/core.py
+++ kyra/core.py
@@ -6,66 +6,66 @@ from datetime import datetime
 
 class Logger(object):
 
-    def __init__(self, filename=None):
-        self._filename = filename
-        self._ERROR = '{0}'.format('\033[91m[ERROR]\033[0m :: ')
-        self._WARNING = '{0}'.format('\033[93m[WARNING]\033[0m :: ')
-        self._INFO = '{0}'.format('\033[94m[INFO]\033[0m :: ')
-        self._NORMAL = '{0}'.format('\033[0m[NORMAL]\033[0m :: ')
-        self._GOOD = '{0}'.format('\033[92m[GOOD]\033[0m :: ')
-        self._FUNCTION = '{0}{1}'.format('\033[95m[FUNCTION]\033[0m', ' :: ')
+    def __init__(self, filename = None):
+        self.__filename = filename
+        self.__ERROR = '{0}'.format('\033[91m[ERROR]\033[0m :: ')
+        self.__WARNING = '{0}'.format('\033[93m[WARNING]\033[0m :: ')
+        self.__INFO = '{0}'.format('\033[94m[INFO]\033[0m :: ')
+        self.__NORMAL = '{0}'.format('\033[0m[NORMAL]\033[0m :: ')
+        self.__GOOD = '{0}'.format('\033[92m[GOOD]\033[0m :: ')
+        self.__FUNCTION = '{0}{1}'.format('\033[95m[FUNCTION]\033[0m', ' :: ')
 
-    def _write(self, filename, content):
+    def __write(self, filename, content):
         with open(os.path.abspath(filename), 'a') as f:
             f.write('{0}{1}'.format(content, '\n'))
 
-    def _formatter(self, error, msg):
-        return('{0}{1}{2}'.format(self._timestamp(), error, msg))
+    def __formatter(self, error, msg):
+        return('{0}{1}{2}'.format(self.__timestamp(), error, msg))
 
-    def _timestamp(self):
+    def __timestamp(self):
         t_current = datetime.now()
         t_ordered = datetime.strftime(t_current, '%d-%m-%Y %H:%M:%S')
         return('{0}{1}'.format(t_ordered, ' :: '))
 
-    def _function_call(self, fn, retval, *args):
-        if self._filename != None:
-            self._write(self._filename, '{0}{1}{2}{3}{4}{5}'.format(self._timestamp(), self._FUNCTION, fn.__name__, retval, ' returned ', str(*args)))
+    def __function_call(self, fn, retval, *args):
+        if self.__filename != None:
+            self.__write(self.__filename, '{0}{1}{2}{3}{4}{5}'.format(self.__timestamp(), self.__FUNCTION, fn.__name__, retval, ' returned ', str(*args)))
         else:
-            print('{0}{1}{2}{3}{4}{5}'.format(self._timestamp(), self._FUNCTION, fn.__name__, retval, ' returned ', str(*args)))
+            print('{0}{1}{2}{3}{4}{5}'.format(self.__timestamp(), self.__FUNCTION, fn.__name__, retval, ' returned ', str(*args)))
 
     def error(self, msg):
-        if self._filename != None:
-            self._write(self._filename, self._formatter(self._ERROR, msg))
+        if self.__filename != None:
+            self.__write(self.__filename, self.__formatter(self.__ERROR, msg))
         else:
-            print(self._formatter(self._ERROR, msg))
+            print(self.__formatter(self.__ERROR, msg))
 
     def warning(self, msg):
-        if self._filename != None:
-            self._write(self._filename, self._formatter(self._WARNING, msg))
+        if self.__filename != None:
+            self.__write(self.__filename, self.__formatter(self.__WARNING, msg))
         else:
-            print(self._formatter(self._WARNING, msg))
+            print(self.__formatter(self.__WARNING, msg))
 
     def info(self, msg):
-        if self._filename != None:
-            self._write(self._filename, self._formatter(self._INFO, msg))
+        if self.__filename != None:
+            self.__write(self.__filename, self.__formatter(self.__INFO, msg))
         else:
-            print(self._formatter(self._INFO, msg))
+            print(self.__formatter(self.__INFO, msg))
 
     def normal(self, msg):
-        if self._filename != None:
-            self._write(self._filename, self._formatter(self._NORMAL, msg))
+        if self.__filename != None:
+            self.__write(self.__filename, self.__formatter(self.__NORMAL, msg))
         else:
-            print(self._formatter(self._NORMAL, msg))
+            print(self.__formatter(self.__NORMAL, msg))
 
     def good(self, msg):
-        if self._filename != None:
-            self._write(self._filename, self._formatter(self._GOOD, msg))
+        if self.__filename != None:
+            self.__write(self.__filename, self.__formatter(self.__GOOD, msg))
         else:
-            print(self._formatter(self._GOOD, msg))
+            print(self.__formatter(self.__GOOD, msg))
 
     def decorator(self, fn):
         def wrapper(*args):
             return_value = fn(*args)
-            self._function_call(fn, return_value, args)
+            self.__function_call(fn, return_value, args)
             return return_value
         return wrapper
